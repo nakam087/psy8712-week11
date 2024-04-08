0061733f043b1c6ab5477e52f02a31e6ffb38284
Sarah C Nakamoto <nakam087@umn.edu>
2024-04-07 23:05:12 -0500

    something is failing :( so im trying something else

--- msi/week11.sh
+++ msi/week11.sh
@@ -5,7 +5,7 @@
 #SBATCH -t 01:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nakam087@umn.edu
-#SBATCH -p amdsmall
+#SBATCH -p msismall
 cd ~/psy8712-week11/msi
 module load R/4.3.0-openblas
 Rscript week11-cluster.R
